98d317c54ee3a96e31f7d0bfeeb4815d784f424e
DuduRPP <dudurpp1@gmail.com>
2022-05-09 22:55:54 -0300

    Logica de secao ativa implementada e Aplicacao Concluida

--- main.js
+++ main.js
@@ -4,17 +4,54 @@ let nav = document.getElementById("navigation");
 let openMenuBtn = document.querySelector(".open-menu");
 let closeMenuBtn = document.querySelector(".close-menu");
 let backToTopButton = document.getElementById("backToTopButton");
+let footer = document.querySelector("footer");
 
 onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  // Linha Alvo
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verificar se a secao passou da linha
+
+  // Topo da Secao
+  const sectionTop = section.offsetTop;
+  //Altura da Secao
+  const sectionHeight = section.offsetHeight;
+
+  // Topo da Secao passou ou alcancou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  // Verificar se a base da Secao esta abaixo da linha alvo
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  // Limites da secao
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 500) {
     backToTopButton.classList.add("show");
-    if (scrollY > 4480) {
+    if (scrollY + innerHeight > footer.offsetTop) {
       backToTopButton.classList.add("onFooter");
     } else {
       backToTopButton.classList.remove("onFooter");
